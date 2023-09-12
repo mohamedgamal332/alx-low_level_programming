b59fe1442ddae5b2f943bac086b238009050886f
mohamednafel <mohamednafel006@gmail.com>
2023-09-12 15:35:02 -0700

    0

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two int
+ *
+ * @a: number of concern
+ * @b: number of concern
+ *
+ * Return: the sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
