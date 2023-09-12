8db424709037034ae2827e8a01d8c051c378e828
mohamednafel <mohamednafel006@gmail.com>
2023-09-11 17:27:55 -0700

    0

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+nclude <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		if (!(c == 'q' || c == 'e'))
+		{
+			putchar(c);
+		}
+	}
+}
