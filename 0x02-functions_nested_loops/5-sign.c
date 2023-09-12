e04cb98133a3eeee3691145e313459c0bddb453a
mohamednafel <mohamednafel006@gmail.com>
2023-09-12 14:18:54 -0700

    0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - accepts a number and tells its sign
+ *
+ * @n: the number of intrest
+ *
+ * Return: returns 1 and 0 and -1 according to the sign
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0')
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
