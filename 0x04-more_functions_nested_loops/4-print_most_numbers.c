9a921512bcfd59c51ea6232b218888f59bd9c658
mohamednafel <mohamednafel006@gmail.com>
2023-09-14 10:25:33 -0700

    0

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
+
+void print_numbers(void)
+{
+        char c;
+
+        for (c = '0'; c <= '9'; c++)
+        {
+	if ( c == '2' || c == '4')
+	{
+		continue;
+	}
+        _putchar(c);
+        }
+        _putchar('\n');
+}
