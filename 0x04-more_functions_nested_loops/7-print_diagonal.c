4f199bc00bc02348338f50df7251f4645ca55b99
mohamednafel <mohamednafel006@gmail.com>
2023-09-14 11:27:38 -0700

    0

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,30 @@
 #include "main.h"
 
 /**
- * print_line - function to print line
+ * print_diagonal - function to print symbols
  * @n : integer value
  * Return: Always 0.
  */
-
-void print_line(int n)
+void print_diagonal(int n)
 {
 	int a, b;
 
 	if (n > 0)
 	{
-		for (a = 0; a < n; a++)
+		for (a = 0; a <= n; a++)
 		{
-			for (b = 0; b < a; b++)
+			for (b = 1; b <= a; b++)
 			{
+				if (b != a)
 				_putchar(' ');
+				else if (b == a)
+				{
+					_putchar('\\');
+					_putchar('\n');
+				}
 			}
-			_putchar('\');
-			_putchar('\n');
 		}
 	}
+	else
 	_putchar('\n');
 }
