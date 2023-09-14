14a1b727cc07d22ee3dbe3ff9efa2c71d9144f1a
mohamednafel <mohamednafel006@gmail.com>
2023-09-14 10:35:41 -0700

    0

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - fun that prints 10x from 0 to 14
+ *
+ * Return: the nums
+ */
+
+void more_numbers(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = '0'; c <= '14'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
