8c5864ec85259410d7d8d2549d66c2819546f076
mohamednafel <mohamednafel006@gmail.com>
2023-09-14 10:10:45 -0700

    0

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - fun that multiply two ints
+ *
+ * @a: int of interist
+ *
+ * @b: int of interist
+ *
+ * Return: the mul result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
