b7168b09de7cb716f49f9a20713f8508832b4480
mohamednafel <mohamednafel006@gmail.com>
2023-09-14 10:15:22 -0700

    0

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the nums
+ *
+ * Return: VOID
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+	_putchar('$')
+}
