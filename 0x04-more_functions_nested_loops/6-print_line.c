37535eea69cb8221794d92ee54fbdd5b537e1b75
mohamednafel <mohamednafel006@gmail.com>
2023-09-14 11:05:44 -0700

    0

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - fun that prints a line
+ *
+ * @n: length of the line
+ *
+ * Return: then line
+ */
+
+void print_line(int n)
+{
+	int j;
+
+	if (n > 0)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
