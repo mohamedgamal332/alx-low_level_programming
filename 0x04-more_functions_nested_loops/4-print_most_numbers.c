726d998fb9029c49ae9a854db5a1be585a02a1b8
mohamednafel <mohamednafel006@gmail.com>
2023-09-14 10:28:21 -0700

    0

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,14 @@
 
 void print_numbers(void)
 {
-        char c;
+	char c;
 
-        for (c = '0'; c <= '9'; c++)
-        {
-	if ( c == '2' || c == '4')
+	for (c = '0'; c <= '9'; c++)
 	{
-		continue;
+	if (!(c == '2' || c == '4'))
+	{
+		_putchar(c);
 	}
-        _putchar(c);
-        }
-        _putchar('\n');
+	}
+	_putchar('\n');
 }
